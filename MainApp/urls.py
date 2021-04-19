193534c405f4ab8e31234cb6d1ef334bc44690e2
Joseph Sherman <shermanator2018@gmail.com>
2021-04-19 14:16:55 -0500

    end of class april 19

--- MainApp/urls.py
+++ MainApp/urls.py
@@ -6,4 +6,7 @@ app_name = 'MainApp'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('topics', views.topics, name='topics'),
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+    path('new_topic/', views.new_topic, name='new_topic'),
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,8 +1,42 @@
 from django.shortcuts import render
 
+from .models import Topic
+from .forms import TopicForm
+
 # Create your views here.
 
 
 def index(request):
     '''The home page for Learning Log. '''
-    return render(request, 'mainapp/index.html')
+    return render(request, 'MainApp/index.html')
+
+
+def topics(request):
+    topics = Topic.objects.order_by('date_added')
+
+    context = {'topics': topics}
+
+    return render(request, 'MainApp/topics.html', context)
+
+
+def topic(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+
+    context = {'topic': topic, 'entries': entries}
+
+    return render(request, 'MainApp/topic.html', context)
+
+
+def new_topic(request):
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)  # maybe not .POST
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('MainApp:topics')
+    context = {'form': form}
+    return render(request, 'MainApp/new_topic.html', context)

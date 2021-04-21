72f776bdc8800aa68f258e7e9acd80a03ecebafa
Joseph Sherman <shermanator2018@gmail.com>
2021-04-21 14:15:01 -0500

    end of class april 21

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 # Create your views here.
 
@@ -32,11 +31,33 @@ def new_topic(request):
     if request.method != 'POST':
         form = TopicForm()
     else:
-        form = TopicForm(data=request.POST)  # maybe not .POST
+        form = TopicForm(data=request.POST)
 
         if form.is_valid():
             form.save()
 
+            # shows topics page with topic they just added
             return redirect('MainApp:topics')
+
     context = {'form': form}
     return render(request, 'MainApp/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+
+            # shows topics page with topic they just added
+            return redirect('MainApp:topic', topic_id=topic_id)
+
+    context = {'form': form, 'topic': topic}
+    return render(request, 'MainApp/new_entry.html', context)
